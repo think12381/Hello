4f0a50ef6c19dabaa8c832878c77dc7e545bac69
think12381 <think123811@gmail.com>
2019-06-12 16:50:40 +0800

    Create Sevice Layer for controller
    controller   <--->  service layer

--- src/main/java/com/example/HelloWorld/Controller/HelloController.java
+++ src/main/java/com/example/HelloWorld/Controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.HelloWorld.Controller;
 
-import com.example.HelloWorld.Message;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import com.example.HelloWorld.Service.Message;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/example/HelloWorld/Service/Message.java
+++ src/main/java/com/example/HelloWorld/Service/Message.java
@@ -1,4 +1,4 @@
-package com.example.HelloWorld;
+package com.example.HelloWorld.Service;
 
 
 public class Message {
